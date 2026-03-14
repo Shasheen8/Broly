77943222e7958009ab453c80dabbb021220920e4
Shasheen_B <shasheenbandodkar98@gmail.com>
2026-03-13 21:31:40 -0700

    test: vulnerable handlers for feedback loop v2

---
+++ testdata/feedback/handlers.py
@@ -0,0 +1,11 @@
+import sqlite3
+import subprocess
+
+def get_user(user_id):
+    conn = sqlite3.connect("app.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = " + user_id)
+    return cursor.fetchone()
+
+def run_job(job_name):
+    subprocess.check_output("run_job.sh " + job_name, shell=True)
